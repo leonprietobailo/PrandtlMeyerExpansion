01869f9833b9db2c3e778f11b90984a052ada160
León <leon.enrique.prieto@estudiantat.upc.edu>
2021-11-25 21:24:17 +0100

    Class creation and update

--- MainProgram/PradntlMeyerExpansion/Grid.cs
+++ MainProgram/PradntlMeyerExpansion/Grid.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Grid
+namespace PradntlMeyerExpansion
 {
     public class Grid
     {
+        List<Cell[]> Mesh = new List<Cell[]>();
+
+        public Cell GetCell(int ve, int ho)
+        {
+            return Mesh[ve][ho];
+        }
+
     }
 }
+
--- MainProgram/PradntlMeyerExpansion/Rules.cs
+++ MainProgram/PradntlMeyerExpansion/Rules.cs
@@ -1,9 +1,55 @@
 ﻿using System;
 
-namespace Rules
+namespace PradntlMeyerExpansion
 {
     public class Rules
     {
-        double u0, v0, ro0, p0, T0, M0;
+        double u0, v0, ro0, p0, T0, M0; // Initial Magnitudes
+        double Cy0, gamma0;
+
+        public Rules(double u, double v, double ro, double p, double T, double M, double Cy, double gamma)
+        {
+            u0 = u;
+            v0 = v;
+            ro0 = ro;
+            p0 = p;
+            T0 = T;
+            M0 = M;
+            Cy0 = Cy;
+            gamma0 = gamma;
+        }
+
+        public double getU()
+        {
+            return u0;
+        }
+        public double getV()
+        {
+            return v0;
+        }
+        public double getRO()
+        {
+            return ro0;
+        }
+        public double getP()
+        {
+            return p0;
+        }
+        public double getT()
+        {
+            return T0;
+        }
+        public double getM()
+        {
+            return M0;
+        }
+        public double getCy()
+        {
+            return Cy0;
+        }
+        public double getGamma()
+        {
+            return gamma0;
+        }
     }
 }
